7a28ae6f2e2d7b3785d120f4bc791415c11806c7
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-05 10:45:23 -0600

    deployed to heroku

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,3 +2,7 @@
 const path = require('path');
 const router = require('express').Router();
 
+// routes
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
