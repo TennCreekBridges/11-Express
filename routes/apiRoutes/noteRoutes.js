50f7228a47569388c39945861f926248838f0eb9
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-05 11:17:02 -0600

    adds GET and POST requests to noteRoutes.js

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,19 @@
+// required
+const router = require('express').Router();
+const notes = require('../../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+// GET request for notes from db.json
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+// POST request to notes at db.json & sets random ID
+router.post('notes', (req, res) => {
+    req.body.id = uuidv4();
+    notes.push(req.body);
+    res.sendFile(__dirname + notes);
+    res.json(req.body);
+});
+
+module.exports = router;
