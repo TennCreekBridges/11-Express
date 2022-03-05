2c7b609ccf94e04f1fc19edccfc7cf0cd14cbf03
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-05 11:11:25 -0600

    changes to apiRoutes index.js

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+// required
+const router = require('express').Router();
+const noteRoutes = require('../apiRoutes/noteRoutes.js');
+
+router.use(note.Routes);
+
+// exports router
+module.exports = router;
